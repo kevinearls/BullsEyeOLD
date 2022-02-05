1de21ab47747c971ff821cd3106af710b9c5ba5b
Kevin Earls <kearls@redhat.com>
2022-02-05 19:16:56 +0100

    Put Hit Me button in its own view, fixed color
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- BullsEye/Views/BullsEyeView.swift
+++ BullsEye/Views/BullsEyeView.swift
@@ -18,30 +18,7 @@ struct BullsEyeView: View {
                 .padding(.leading)
                 .padding(.trailing)
             SliderView(sliderValue: $sliderValue)
-            Button(action: {
-                self.alertIsVisible = true
-            }) {
-                Text("Hit Me")
-            }
-            .alert(
-                "Hello there!",
-                isPresented: $alertIsVisible,
-                presenting: {
-                  let roundedValue = Int(sliderValue.rounded())
-                  return (
-                    roundedValue,
-                    game.points(sliderValue: roundedValue)
-                  )
-                } () as (roundedValue: Int, points: Int)
-              ) { data in
-                Button("Awesome!") {
-                  game.startNewRound(points: data.points)
-                }
-              } message: { data in
-                  Text("The slider's value is \(data.roundedValue).\n" + "You scored \(data.points) points this round.")
-                  Text("This is the message part")
-              }
-             
+            HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
         }
     }
     
@@ -67,6 +44,36 @@ struct BullsEyeView: View {
             }
             .padding()
         }
+    }
+    
+    struct HitMeButton: View {
+        @Binding var alertIsVisible: Bool
+        @Binding var sliderValue: Double
+        @Binding var game: Game
+        
+        var body: some View {
+            Button(action: {
+              print("Hello, SwiftUI!")
+              alertIsVisible = true
+            }) {
+              Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+            }
+              .padding(20.0)
+              .background(
+                ZStack {
+                  Color("ButtonColor")
+                  LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                }
+              )
+              .foregroundColor(Color.white)
+              .cornerRadius(21.0)
+              .alert(isPresented: $alertIsVisible, content: {
+                let roundedValue = Int(sliderValue.rounded())
+                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+              })
+          }
     }
 }
 
